ed3e984d885eecb0e3ef237486f48ab1513d653f
ConnorHClark <connor@henriclark.com>
2022-11-16 09:22:12 +0000

    Update HardwareWalletIdentifier.py
    Print all previously connected devices output to terminal.

--- HardwareWalletIdentifier.py
+++ HardwareWalletIdentifier.py
@@ -4,7 +4,18 @@ def main():
     access_registry = winreg.ConnectRegistry(None,winreg.HKEY_LOCAL_MACHINE)
     access_key = winreg.OpenKey(access_registry,r'SYSTEM\ControlSet001\Enum\USB')
     
+    connected_devices = []
 
+    print("Previously connected devices:")
+    print("----------------------------------------")
+    for n in range(1000):
+        try:
+            x = winreg.EnumKey(access_key,n)
+            print(x)
+            
+        except:
+            break
+    print("----------------------------------------")
 
 if __name__ == '__main__':
     main()
